dcf3f6e2c68a8ee0540f1f04e9cd86027b3da495
BenR <20133902+thisbenrogers@users.noreply.github.com>
2019-07-22 15:24:18 -0500

    :art: Converts SavedList items to NavLinks

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => {
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
       {props.list.map((movie, index) => (
-        <Link to={`/movies/${index}`} key={index} className="saved-movie">{movie.title}</Link>
-      ))}
+        <NavLink to={`/movies/${movie.id}`} key={index} className="saved-movie">{movie.title}</NavLink>
+      ))
+      }
       <Link to="/">
         <div className="home-button">Home</div>
       </Link>
-    </div>
+    </div >
   )
 };
 
